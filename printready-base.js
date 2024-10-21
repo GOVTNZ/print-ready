b98563cd09edd2470a43bab0848e0d4ec78bdb43
benwrighton <benjaminwrighton@gmail.com>
2024-10-21 14:23:51 +1300

    Deploy PrintReady v1.0.0
    Initial GitHub Pages deployment of PrintReady toolkit documentation and live demo examples from main branch tag v1.0.0.

---
+++ printready-base.js
@@ -0,0 +1,264 @@
+/**
+ * Initializes a print button that triggers the print dialog when clicked.
+ * @param {string} buttonSelector - The CSS selector for the print button.
+ */
+export function initializePrintButton(buttonSelector) {
+  const printButton = document.querySelector(buttonSelector);
+
+  if (printButton) {
+
+    if (typeof window.print === 'function') {
+      printButton.addEventListener('click', (event) => {
+        window.print();
+        event.preventDefault();
+      });
+      printButton.hidden = false;
+    } else {
+      console.error('window.print is not available in this environment.');
+    }
+
+  } else {
+    console.warn('Print button not found');
+  }
+}
+
+
+/**
+ * Creates and returns a DOM element representing agency information for printing.
+ * @param {string} agency - The name to be displayed in the printable information.
+ * @returns {HTMLElement} - A DOM element containing the agency information.
+ */
+export function showAgency(agency) {
+  if (typeof agency !== 'string' || agency.trim() === '') {
+    console.error('Invalid agency name provided to showAgency.');
+    return null;
+  }
+
+  const container = document.createElement('div');
+  container.className = 'printready-agency print-only';
+
+  const paragraph = document.createElement('p');
+  paragraph.className = 'print-info-agency';
+
+  const bold = document.createElement('b');
+  bold.textContent = agency;
+
+  paragraph.appendChild(bold);
+  container.appendChild(paragraph);
+
+  return container;
+}
+
+
+/**
+ * Creates and returns a DOM element containing printable page information.
+ * Uses the document's <title> tag for the page title.
+ * @returns {HTMLElement} - A DOM element with page title, current date, and page URL for printing.
+ */
+export function showPageInformation() {
+  let webpageUrl = undefined;
+  
+  if (typeof window.location !== 'object') {
+    console.error('window.location is unavailable.');
+  } else {
+    webpageUrl = window.location.href
+  }
+  
+  const pageTitle = document.title || 'Untitled Page';
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+  const currentDate = new Date().toLocaleDateString('en-GB', options);
+
+  if (pageTitle === 'Untitled Page') {
+    console.warn('No page title found');
+  }
+
+  const container = document.createElement('div');
+  container.className = 'printready-page-info print-only';
+
+  const titlePara = document.createElement('p');
+  titlePara.className = 'print-info-title';
+  titlePara.innerHTML = `<b>Page title:</b> ${pageTitle}`;
+
+  const datePara = document.createElement('p');
+  datePara.className = 'print-info-date';
+  datePara.innerHTML = `<b>Printed:</b> ${currentDate}`;
+
+  container.appendChild(titlePara);
+  container.appendChild(datePara);
+  
+  if ( webpageUrl !== undefined ) {
+    const urlPara = document.createElement('p');
+    urlPara.className = 'print-info-url';
+    urlPara.innerHTML = `<b>Printed from:</b> ${webpageUrl}`;
+    container.appendChild(urlPara);
+  }  
+
+
+  return container;
+}
+
+
+/**
+ * Generates a list of printable links.
+ * @param {string} [linksSelector=null] - The CSS selector for the links to be included.
+ * @param {string} [excludeLinksSelector=null] - The CSS selector for the links to be excluded.
+ * @param {boolean} [externalOnly=false] - Whether to  only include external links.
+ * @returns {string} - The HTML string with the list of printable links.
+ */
+export function generateListOfPageLinks(
+  linksSelector = null,
+  excludeLinksSelector = null,
+  externalOnly = false
+) {
+  if (!linksSelector) {
+    console.warn('No links selector provided');
+    return null;
+  }
+
+  document.querySelectorAll(undefined).length
+
+  let selectedLinks = document.querySelectorAll(linksSelector);
+  
+  if (!selectedLinks.length) {
+    console.warn('No elements found matching the provided linksSelector.');
+    return null;
+  }
+
+  if (excludeLinksSelector) {
+    selectedLinks = Array.from(selectedLinks).filter(link => !link.matches(excludeLinksSelector));
+  }
+  
+    if (!selectedLinks.length) {
+    console.warn('No elements remain after applying excludeLinksSelector filter.');
+    return null;
+  }
+
+  const linksToPrint = [];
+  let referenceNumber = 1;
+  
+  selectedLinks.forEach((linkElement) => {
+    const formattedLink = handleLink(linkElement, externalOnly);
+    if (formattedLink) {
+      addReferenceToLink(linkElement, referenceNumber);
+      linksToPrint.push(formattedLink);
+      referenceNumber++;
+    }
+  });
+  
+  if (!linksToPrint.length) {
+    console.warn('No valid links found to print after processing the selected elements.');
+    return null;
+  }
+
+  return outputPrintedLinks(linksToPrint);
+}
+
+/**
+ * Outputs the list of printable links as HTML string.
+ * @param {Array} linksToPrint - The array of links to be printed.
+ * @returns {string} - The HTML string with the list of printable links.
+ */
+function outputPrintedLinks(linksToPrint) {
+  const ol = document.createElement('ol');
+  ol.className = 'js-printready-links-list';
+
+  linksToPrint.forEach(link => {
+    const li = document.createElement('li');
+    li.textContent = link;
+    ol.appendChild(li);
+  });
+
+  return ol;
+}
+
+
+
+/**
+ * Adds a reference number to the link element for printing.
+ * @param {Element} linkElement - The link element to which the reference number will be added.
+ * @param {number} refNum - The reference number to be added.
+ */
+function addReferenceToLink(linkElement, refNum) {
+  linkElement.classList.add('js-hasPrintLinkRef');
+
+  const sup = document.createElement('sup');
+  sup.className = 'print-only js-printready-link-reference';
+  sup.textContent = `[Link: ${refNum}]`;
+
+  linkElement.insertAdjacentElement('afterend', sup);
+}
+
+
+/**
+ * Handles the link element and returns the formatted link based on its type.
+ * @param {Element} linkElement - The link element to be handled.
+ * @param {boolean} externalOnly - Whether to include only external links.
+ * @returns {string|null} - The formatted link or null if not applicable.
+ */
+function handleLink(linkElement, externalOnly) {
+  const href = linkElement.href; 
+  const baseUrl = window.location.origin;
+
+  if (!href || href.startsWith("#")) return null;
+
+  let isExternalOrSpecialLink = null;
+
+  // Check link is not internal
+  if ( !href.startsWith(baseUrl) ) { 
+    isExternalOrSpecialLink = true; 
+  }
+  
+  if (externalOnly && !isExternalOrSpecialLink) return null;
+
+  const handlers = {
+    'http': handleExternalLink, // this also covers 'https'
+    'tel:': handleTelLink,
+    'mailto:': handleMailtoLink
+  };
+
+  for (const key in handlers) {
+    if (href.startsWith(key)) {
+      return handlers[key](href);
+    }
+  }
+
+  return null;
+}
+
+/**
+ * Handles external links (http/https) and returns the link URL.
+ * @param {string} href - The URL of the external link.
+ * @returns {string} - The URL of the external link.
+ */
+function handleExternalLink(href) {
+  return href;
+}
+
+/**
+ * Handles telephone links and returns the formatted phone number.
+ * @param {string} href - The URL of the telephone link.
+ * @returns {string} - The formatted phone number.
+ */
+function handleTelLink(href) {
+  const phoneNumber = href.replace('tel:', '').trim();
+  return `Phone number: ${phoneNumber}`;
+}
+
+/**
+ * Handles mailto links and returns the formatted email address and subject.
+ * @param {string} href - The URL of the mailto link.
+ * @returns {string} - The formatted email address and subject.
+ */
+function handleMailtoLink(href) {
+  const [email, query] = href.replace('mailto:', '').split('?');
+  let output = `Email: ${email}`;
+
+  if (query) {
+    const params = new URLSearchParams(query);
+    if (params.has('subject')) {
+      output += ` - Subject: ${params.get('subject')}`;
+    }
+  }
+
+  return output;
+}
---
+++ printready-site.js
@@ -0,0 +1,83 @@
+// Importing functions from the 'printready-base' file
+import { showAgency, initializePrintButton, showPageInformation, generateListOfPageLinks } from './printready-base.js';
+
+// Initialize the DOMContentLoaded event listener to ensure the script runs after the page is fully loaded (optional)
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Initialize print button if it exists on the page. This is for progressive enhancement and assumes the button is rendered with the 'hidden' html attribute.  
+    const printButtonSelector = "#print-page-button"; // Selector for print button - update if your print button has a different HTML ID or class. 
+    
+    if (document.querySelector(printButtonSelector)) {
+        initializePrintButton(printButtonSelector);
+    }
+
+    // Insert printable page information at the top of the document (optional, can use a logo instead, see README.md for more details)
+    // You can choose to display a print friendly version of your organisations logo instead.
+    const agencyName = "PrintReady Example Site"; // Replace with the name of your site when printing.
+    const agencyComponent = showAgency(agencyName); 
+
+    if( agencyComponent !== null ) {
+        document.body.prepend(showAgency(agencyName));
+    } 
+   
+    // Insert printable page information at the bottom of the document (optional, see README.md for more details)
+    document.body.appendChild(showPageInformation());
+  
+    // Initialize a variable for the list of links
+    let listOfLinks = null;
+
+    // Determine which links to include/exclude based on page type or specific needs
+    // A switch statement is used here to allow for different links to be selected for different types of pages. 
+    switch (true) {
+        // Case for a specific page type (e.g., homepage)
+        case document.body.classList.contains('pagetype-home'):
+            listOfLinks = generateListOfPageLinks(
+                '.homepage-content a', // Selector for links to include
+                null,                // No exclusion selector
+                false                 // Only include external links
+            );
+            break;
+
+        // Default case for other page types
+        default:
+            listOfLinks = generateListOfPageLinks(
+                '.main-content a', // Links to include
+                '.sidenav a', // Links to exclude
+                false // Only include external links, setting to 'false' will also include internal links. Mailto and Tel links are always included, anchor links are always excluded  
+            );
+            break;
+    }
+
+    if (listOfLinks !== "" && listOfLinks !== null) {
+        // Define the printed links section which lists the URLs of the selected links
+        // Change this HTML structure if necessery.   
+        const printedLinksSection = document.createElement('div');
+        printedLinksSection.id = 'js-printready-link-urls';
+        printedLinksSection.className = 'print-only';
+
+        const heading = document.createElement('h2');
+        heading.textContent = 'Index of page links';
+
+        printedLinksSection.appendChild(heading);
+        printedLinksSection.appendChild(listOfLinks);
+
+        // Define the target element to insert the printed links section after.
+        // You can change this to suit your site. 
+        let renderTarget = '.main-content';
+        
+        // OPTIONAL - You can use a switch statement to have different render targets for particular pagetypes.  
+        // EXAMPLE: Set the render target based on the page type
+        // if (document.body.classList.contains('pagetype-home')) {
+        //     renderTarget = '.homepage-content';
+        // }
+
+        // Insert the printed links section after the renderTarget if it exists.
+        if (renderTarget !== '' && document.querySelector(renderTarget) !== null) {
+            const renderTargetNode = document.querySelector(renderTarget);
+            renderTargetNode.parentNode.insertBefore(printedLinksSection, renderTargetNode.nextSibling);
+        } else {
+            console.warn('No render target found for the printed links section.');
+        }
+
+    }
+});
