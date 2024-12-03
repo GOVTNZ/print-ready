6e69333f9a684238397e44891b5c8fcdd63707d1
Ben Wrighton <ben.wrighton@dia.govt.nz>
2024-12-03 17:20:11 +1300

    Add test for external link.

--- tests/printReady.spec.js
+++ tests/printReady.spec.js
@@ -17,6 +17,18 @@ test('Empty link is ignored', async ({ page }) => {
   await expect(oldSchoolAnchor).toHaveText('Empty link');
 });
 
+test('Check External link is listed', async ({ page }) => {
+  await page.goto('/'); 
+
+  const link = page.getByRole('link', { name: 'External link' });
+  await expect(link).toHaveClass(/js-hasPrintLinkRef/); // TODO make exact match not regex. 
+  
+  await expect(page.locator('.js-printready-links-list')).toContainText('http://www.google.com/') ;
+
+  // TODO check this was an adjacent <sup>?
+  // await expect(page.getByRole('link', { name: 'External link' })); 
+
+});
 // test('get started link', async ({ page }) => {
 //   await page.goto('https://playwright.dev/');
 
